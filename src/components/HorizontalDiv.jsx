cbfc7d55011d1199aee662600f4434b7c1d1b155
rabbykhairul <rabbykhairul@gmail.com>
2021-04-19 23:21:23 +0600

    HorizontalDiv - make all child divs zoomable

--- src/components/HorizontalDiv.jsx
+++ src/components/HorizontalDiv.jsx
@@ -48,14 +48,30 @@ const HorizontalDiv = (props) => {
       <QuickPinchZoom onUpdate={onZoomChange} tapZoomFactor={0} draggableUnZoomed={false}>
         <div className="child-item bg-e63946" ref={annotationRef}>1</div>
       </QuickPinchZoom>
-      <div className="child-item bg-f1faee">2</div>
-      <div className="child-item bg-a8dadc">3</div>
-      <div className="child-item bg-457b9d">4</div>
-      <div className="child-item bg-1d3557">5</div>
-      <div className="child-item bg-d00000">6</div>
-      <div className="child-item bg-03071e">7</div>
-      <div className="child-item bg-ffba08">8</div>
-      <div className="child-item bg-f8f9fa">9</div>
+      <QuickPinchZoom onUpdate={onZoomChange} tapZoomFactor={0} draggableUnZoomed={false}>
+        <div className="child-item bg-f1faee">2</div>
+      </QuickPinchZoom>
+      <QuickPinchZoom onUpdate={onZoomChange} tapZoomFactor={0} draggableUnZoomed={false}>
+        <div className="child-item bg-a8dadc">3</div>
+      </QuickPinchZoom>
+      <QuickPinchZoom onUpdate={onZoomChange} tapZoomFactor={0} draggableUnZoomed={false}>
+        <div className="child-item bg-457b9d">4</div>
+      </QuickPinchZoom>
+      <QuickPinchZoom onUpdate={onZoomChange} tapZoomFactor={0} draggableUnZoomed={false}>
+        <div className="child-item bg-1d3557">5</div>
+      </QuickPinchZoom>
+      <QuickPinchZoom onUpdate={onZoomChange} tapZoomFactor={0} draggableUnZoomed={false}>
+        <div className="child-item bg-d00000">6</div>
+      </QuickPinchZoom>
+      <QuickPinchZoom onUpdate={onZoomChange} tapZoomFactor={0} draggableUnZoomed={false}>
+        <div className="child-item bg-03071e">7</div>
+      </QuickPinchZoom>
+      <QuickPinchZoom onUpdate={onZoomChange} tapZoomFactor={0} draggableUnZoomed={false}>
+        <div className="child-item bg-ffba08">8</div>
+      </QuickPinchZoom>
+      <QuickPinchZoom onUpdate={onZoomChange} tapZoomFactor={0} draggableUnZoomed={false}>
+        <div className="child-item bg-f8f9fa">9</div>
+      </QuickPinchZoom>
     </div>
   );
 }
